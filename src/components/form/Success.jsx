bfeaf6ae6d8247637d75cb78534a34c3950b5322
Ekaraja <75461147+Ekaraja@users.noreply.github.com>
2022-07-11 12:35:59 +0200

    display error messages when field are unvalid

--- src/components/form/Success.jsx
+++ src/components/form/Success.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Success = ({ formData }) => {
 	return (
 		<div className="form__success-message">
-			<h4>Good job {formData.firstname}</h4>
+			<h4>Good job {formData.firstname}!</h4>
 			<p>We have sent you an email to <span className="--bold">{formData.email}</span>.<br/>Make sure to click the link from the message to activate your account.</p>
-			<button className="button">Go to homepage</button>
+			<a href="./" className="button">Go to homepage</a>
 		</div>
 	);
 };
